1d7c25a9040befa55a02bfb7949ca2843617f6f2
Thiago Canudo <thiago@thiagocanudo.com.br>
2016-12-14 02:46:30 -0200

    refatoracao js

--- js/search.js
+++ js/search.js
@@ -1,21 +1,38 @@
-$('#btConsulta').click(function(e) {
-  e.preventDefault();
-  $btConsulta = $('#btConsulta');
-  $cep = $('#cep').val();
-  $resposta = $('h2');
-  $resposta.addClass('load').text('');
-  $.getJSON("http://cep.republicavirtual.com.br/web_cep.php?cep=" + $cep + "&formato=json", {}, function(data) {
-    if (data.resultado_txt == 'sucesso - cep completo') {
-      $resposta.text(data.tipo_logradouro + ' ' + data.logradouro + ', ' + data.bairro + ', ' + data.cidade + ' - ' + data.uf);
-      $resposta.removeClass('load');
-    }else{
-      if($cep.length===0){
-        $resposta.text('Digite o CEP para consulta');
+$('document').ready(function(){
+  $('#cep').focus();
+
+  var $btConsulta = $('#btConsulta');
+  var $resposta = $('h2');
+  var consulta = function(){
+  var $cep = $('#cep').val();
+    $resposta.addClass('load').text('');
+    $.getJSON("http://cep.republicavirtual.com.br/web_cep.php?cep=" + $cep + "&formato=json", {}, function(data) {
+      if (data.resultado_txt == 'sucesso - cep completo') {
+        $resposta.text(data.tipo_logradouro + ' ' + data.logradouro + ', ' + data.bairro + ', ' + data.cidade + ' - ' + data.uf);
         $resposta.removeClass('load');
       }else{
-        $resposta.text('Cep inválido ou não encontrado');
-        $resposta.removeClass('load');
+        if($cep.length===0){
+          $resposta.text('Digite o CEP para consulta');
+          $resposta.removeClass('load');
+        }else{
+          $resposta.text('Cep inválido ou não encontrado');
+          $resposta.removeClass('load');
+        }
       }
+    });
+  };
+
+  $btConsulta.click(function(e){
+    e.preventDefault();
+    consulta();
+  });
+
+  $(document).keyup(function(e) {
+    if(event.keyCode == 13) {
+        consulta();   
     }
   });
+
 });
+
+
